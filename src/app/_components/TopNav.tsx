6d0baa55b2177583a5a1f4e1e40c0da322f28bd9
BenyaOps <benyaminmit147@gmail.com>
2025-02-08 07:09:12 -0500

    sign in sign up

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,17 +1,21 @@
 import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
-import { UploadButton } from '~/utils/uploadthing'
+// import { UploadButton } from '~/utils/uploadthing'
 import { SimpleUploadButton } from './simole-upload-button'
+import LogoIcon from './svg/Logo'
+import TicketIcon from './svg/Ticket'
 
 const TopNav = () => {
   return (
     <nav className='flex justify-between items-center p-4 bg-gray-800 text-white'>
-        <div className='text-xl font-bold'>Logo</div>
+        <div className='text-xl font-bold'>
+          <LogoIcon />  
+        </div>
         <div>
             <ul className='flex space-x-4'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li><TicketIcon/> Eventos</li>
+            <li><TicketIcon/>Calendario</li>
+            <li><TicketIcon/>Proximamente</li>
             </ul>
         </div>
         <div className='flex flex-row'>
